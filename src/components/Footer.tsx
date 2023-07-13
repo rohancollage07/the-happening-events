56fa0a0ddbd05ceb89c583c109eeef9ff2b886c0
Rohan Dongre <rohancollege07@gmail.com>
2023-07-13 20:22:26 +0530

    Now check

---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+
+// import React from 'react';
+import '../pages/Events.css'; 
+
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-section">
+        <h3>Address and Phone Number</h3>
+        <p>123 Main Street, City, Country</p>
+        <p>Phone: +1234567890</p>
+      </div>
+      <div className="footer-section">
+        <h3>Useful Links</h3>
+        <ul>
+          <a href=""><li>Home</li></a>
+          <a href=""><li>About Us</li></a> 
+          <a href=""><li>Events</li></a> 
+          <a href=""><li>Contact Us</li></a> 
+
+          
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h3>Social Media Links</h3>
+        <ul>
+          <a href=""><li>FaceBoook</li></a> 
+          <a href=""><li>Twitter</li></a> 
+          <a href=""><li>Instagram</li></a> 
+          <a href=""><li>LinkedIn</li></a> 
+        </ul>
+      </div>
+    </footer>
+  );
+
+
+export default Footer
